c9cccf6cd1341bf176138ced6415ca0a61a614f4
Lane Fong <547791764@qq.com>
2022-04-05 23:53:28 +0800

    chore: auth login

--- src/app.js
+++ src/app.js
@@ -1,19 +1,53 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Taro from '@tarojs/taro'
 
-import { Button, Toast,Tabbar,TabbarItem,Icon ,Avatar } from '@nutui/nutui-taro';
 
+import { Button,Radio,OverLay,Picker,Popup,DatePicker,RadioGroup,Tabs, TabPane, Toast,Form,FormItem,Icon ,Cell,CellGroup,Avatar ,Input} from '@nutui/nutui-taro';
 
+import '@tarojs/taro/html.css';
+import 'normalize.css/normalize.css';
 import './app.scss'
+import './style/iconfont.css'
+// import "@nutui/nutui-taro/dist/style.css";
+
+const checkSession = () => {
+ return wx.checkSession()
+}
+
+const doLogin = async() => {
+  // const sessionErrMsg = await checkSession()
+  const token = Taro.getStorageSync('token')
+  // console.log('[ sessionErrMsg ] >', sessionErrMsg)
+  if ( !token) {
+    Taro.reLaunch({
+      url: '/pages/index/index'
+    })
+  }else{
+
+    Taro.reLaunch({
+      url: '/pages/profile/index'
+    })
+  }
+}
+
 
 const App = createApp({
-  onShow (options) {},
-  // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
+  onLaunch(){
+    doLogin()
+  }
 })
 
 
 
 
-App.use(Button).use(Toast).use(Tabbar).use(TabbarItem).use(Icon).use(Avatar)
+App.use(Button).use(Form)
+.use(FormItem).use(Icon).use(Avatar)
+.use(Cell).use(CellGroup)
+.use(Radio).use(RadioGroup)
+.use(Input).use(Popup).use(OverLay)
+.use(Tabs).use(TabPane)
+.use(DatePicker).use(Picker)
+
 App.use(createPinia())
 export default App
