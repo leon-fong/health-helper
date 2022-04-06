21b43ea2f613d67cdb0c80040b23f291e229b6f4
Lane Fong <547791764@qq.com>
2022-04-06 19:50:34 +0800

    chore: app.config

--- src/app.config.js
+++ src/app.config.js
@@ -35,7 +35,7 @@ export default defineAppConfig({
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
+    navigationBarTitleText: ' ',
     navigationBarTextStyle: 'black'
   }
 })
