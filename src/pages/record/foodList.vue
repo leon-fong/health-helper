a41591a7d3539c606254f5315ab424228a32101e
Lane Fong <547791764@qq.com>
2022-04-06 19:51:01 +0800

    feat: foodList layout

--- src/pages/record/foodList.vue
+++ src/pages/record/foodList.vue
@@ -14,6 +14,11 @@ getFoodList().then((res) => {
     console.log('[ list ] >', res.data.items)
   }
 })
+const currentId = ref('')
+const handleSelect = (id) => {
+  currentId.value = id
+  console.log('[ handleSelect ] >', id)
+}
 
 const handleSubmit = () => {
   // Todo
@@ -22,15 +27,15 @@ const handleSubmit = () => {
 
 <template>
   <div class="food-list">
-    <div v-for="item in foodList" :key="item.id">
-      <div class="list box">
+    <div v-for="item in foodList" :key="item.id" @click="handleSelect(item.id)">
+      <div class="list box" :class="{ active: currentId === item.id }">
         <div class="left">
           <nut-avatar size="normal" style="vertical-align: middle" :icon="item.path"></nut-avatar>
           <span class="ellipsis" style="margin-left: 10px">{{ item.name }}</span>
         </div>
         <div class="right">
           <span style="color: #f00">{{ item.attrs.calories.value }}</span>
-          <span style="color: #666"> 千卡</span>
+          <span style="opacity: 0.5"> 千卡</span>
         </div>
       </div>
     </div>
@@ -44,6 +49,8 @@ const handleSubmit = () => {
 .food-list {
   padding: 20px;
   width: 100%;
+  padding-bottom: constant(safe-area-inset-bottom + 60px);
+  padding-bottom: calc(env(safe-area-inset-bottom) + 40px);
   box-sizing: border-box;
   .list {
     width: 100%;
@@ -51,13 +58,19 @@ const handleSubmit = () => {
     justify-content: space-between;
     align-items: center;
   }
-  .submit {
-    width: 100%;
-    position: fixed;
-    bottom: 0;
-    padding-left: 20px;
-    padding-right: 20px;
-    box-sizing: border-box;
+  .active {
+    background-color: #eff8e9;
+    color: #6ab840;
   }
+}
+.submit {
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  background-color: #f4f4f4;
+  padding-left: 20px;
+  padding-right: 20px;
+  box-sizing: border-box;
 }
 </style>
