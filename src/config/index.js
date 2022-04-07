f772e896353a61f60ee3316a36abeb8f2acfc409
Lane Fong <547791764@qq.com>
2022-04-07 22:36:24 +0800

    chore: replace baseUrl

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export const baseUrl = process.env.NODE_ENV === 'development' ? 
 // 'http://114.96.78.144:8088/health' : './'
-'http://xlight.vaiwan.cn/health': './'
+'http://light-x.vaiwan.cn/health': './'
 
