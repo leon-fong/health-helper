5319d8ddf56bb3db815148af88d9820f04072171
Lane Fong <547791764@qq.com>
2022-04-10 22:21:04 +0800

    feat: add report page

--- src/pages/profile/report.vue
+++ src/pages/profile/report.vue
@@ -1,11 +1,42 @@
 <template>
-  <div class="eat-suggestion">report</div>
+  <div class="eat-suggestion">
+    <h3>血压情况</h3>
+    <div class="box">{{ report.blood?.bloodPressureDesc }}</div>
+    <h3>血糖情况</h3>
+    <div class="box">{{ report.blood?.bloodSugarDesc }}</div>
+    <h3>BMI</h3>
+    <div class="bmi box">
+      {{ report.weight?.bmi }}
+      <span class="desc">{{ report.weight?.bmiDesc }}</span>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import Taro from '@tarojs/taro'
+import { ref } from 'vue'
 import { healthReport } from '~@/apis/profile.js'
-healthReport()
+const report = ref({})
+healthReport().then((res) => {
+  if (res.code === 0) {
+    report.value = res.data
+  }
+})
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.eat-suggestion {
+  padding: 20px;
+  .box {
+    text-align: center;
+    color: $primary-color;
+    font-size: 16px;
+  }
+  .bmi {
+    .desc {
+      margin-left: 5px;
+      color: #ccc;
+      font-size: 13px;
+    }
+  }
+}
+</style>
