7fb876b2e913c8a1dbf047669ec1a7d7e3924c7e
Lane Fong <547791764@qq.com>
2022-04-25 10:46:31 +0800

    feat: report

--- src/pages/profile/report.vue
+++ src/pages/profile/report.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="eat-suggestion">
+  <div class="eat-suggestion" v-if="isShow">
     <h3>血压情况</h3>
     <div class="box">{{ report.blood?.bloodPressureDesc }}</div>
+    <!-- <div class="box">
+      <b>建议：</b>
+    </div> -->
     <h3>血糖情况</h3>
     <div class="box">{{ report.blood?.bloodSugarDesc }}</div>
     <h3>BMI</h3>
@@ -10,15 +13,33 @@
       <span class="desc">{{ report.weight?.bmiDesc }}</span>
     </div>
   </div>
+  <div v-else style="text-align: center">
+    <h3>{{ tips }}</h3>
+  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { healthReport } from '~@/apis/profile.js'
+
+// todo: 健康建议
+// const suggestion = {
+//   高血糖
+//   低血糖
+//   偏瘦
+//   偏胖
+// }
+
 const report = ref({})
+const isShow = ref(false)
+const tips = ref('')
 healthReport().then((res) => {
   if (res.code === 0) {
     report.value = res.data
+    isShow.value = true
+  } else {
+    isShow.value = false
+    tips.value = res.msg
   }
 })
 </script>
