8c03249692fb46fdfbd939b1dc04b20568e84f60
Lane Fong <547791764@qq.com>
2022-04-25 10:32:04 +0800

    feat: add weight history list

--- src/pages/analysis/index.vue
+++ src/pages/analysis/index.vue
@@ -1,103 +1,173 @@
 <template>
+  <nut-tabs v-model="selected" @change="tabChange">
+    <nut-tabpane v-for="(value, key) in type" :key="key" :paneKey="key" :title="value"> </nut-tabpane>
+  </nut-tabs>
   <view class="record">
-    <e-chart ref="chartRef" canvas-id="bar-canvas" />
+    <div class="toggle">
+      <nut-button type="primary" size="mini" v-if="selected === 2" @click="toggle">{{ toggleTitle }}</nut-button>
+    </div>
+    <div style="padding: 20px" v-if="selected === 2 && isShowTable">
+      <nut-table :columns="state.columns" :data="state.data"></nut-table>
+      <div class="more" @click="getMore">查看更多</div>
+    </div>
+    <e-chart v-show="!isShowTable || selected !== 2" ref="chartRef" canvas-id="bar-canvas" />
   </view>
 </template>
 
 <script setup>
-import { computed, ref } from 'vue'
+import { computed, ref, reactive } from 'vue'
 import _ from 'lodash'
-import { getAnalysis } from '~@/apis/analysis.js'
+import dayjs from 'dayjs'
+import { getAnalysis, getWeightList } from '~@/apis/analysis.js'
 import Taro from '@tarojs/taro'
 import { EChart } from 'echarts4taro3'
-const list = ref({})
-getAnalysis().then((res) => {
-  if (res.code === 0) {
-    list.value = res.data.items
-  }
+
+const state = reactive({
+  columns: [
+    {
+      title: '日期',
+      key: 'reportDate',
+    },
+    {
+      title: '体重',
+      key: 'weight',
+    },
+    {
+      title: '幅度',
+      key: 'gain',
+    },
+  ],
+  data: [],
 })
 
-const weightArr = ref([])
-const bloodSugarArr = ref([])
-const highBloodPressureArr = ref([])
-const lowBloodPressureArr = ref([])
-const dateArr = computed(() => {
-  const arr = _.keys(list.value).sort()
-  arr.forEach((item) => {
-    weightArr.value.push(list.value[item].weight)
-    bloodSugarArr.value.push(list.value[item].bloodSugar)
-    highBloodPressureArr.value.push(list.value[item].highBlood)
-    lowBloodPressureArr.value.push(list.value[item].lowBloodPressure)
+const isShowTable = ref(false)
+const toggle = () => {
+  isShowTable.value = !isShowTable.value
+}
+
+const toggleTitle = computed(() => {
+  return isShowTable.value ? '折线图' : '列表'
+})
+
+const type = {
+  bloodSugar: '血糖',
+  bloodPressure: '血压',
+  weight: '体重',
+}
+
+const option = {
+  legend: {
+    data: [],
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '10%',
+
+    containLabel: true,
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      // 坐标轴指示器，坐标轴触发有效
+      type: 'shadow', // 默认为直线，可选为：'line' | 'shadow'
+    },
+  },
+  xAxis: {
+    type: 'category',
+    data: [],
+  },
+  yAxis: {
+    type: 'value',
+  },
+  series: [],
+}
+
+const getMore = () => {
+  // Todo 查看更多
+}
+
+const selected = ref(0)
+let date = []
+let data = []
+function init(key) {
+  return new Promise((resolve, reject) => {
+    getAnalysis(key).then((res) => {
+      if (res.code === 0) {
+        date = Object.keys(res.data)
+        const list = Object.values(res.data)
+        if (key === 'bloodPressure') {
+          data = {
+            high: [],
+            low: [],
+          }
+          list.map((item) => {
+            data.high.push(item.highBloodPressure)
+            data.low.push(item.lowBloodPressure)
+          })
+          option.series = [
+            {
+              name: '血压(高)',
+              type: 'line',
+              stack: 'Total',
+              data: data.high,
+            },
+            {
+              name: '血压(低)',
+              type: 'line',
+              stack: 'Total',
+              data: data.low,
+            },
+          ]
+          option.legend.data = ['血压(高)', '血压(低)']
+        } else {
+          data = list
+          option.series = [
+            {
+              name: type[key],
+              type: 'line',
+              stack: 'Total',
+              data: data,
+            },
+          ]
+          option.legend.data = [type[key]]
+        }
+        option.xAxis.data = date
+        chartRef.value.setOption(option)
+        resolve()
+      }
+    })
   })
+}
+init('bloodSugar')
+getWeightList().then((res) => {
+  if (res.code === 0) {
+    res.data.items.map((item) => {
+      item.reportDate = dayjs(item.reportDate).format('YYYY-MM-DD')
+    })
 
-  return arr
-})
-
-const option = computed(() => {
-  return {
-    legend: {
-      data: ['体重', '血糖', '血压(高)', '血压(低)'],
-    },
-    grid: {
-      left: '3%',
-      right: '4%',
-      bottom: '10%',
-      containLabel: true,
-    },
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {
-        // 坐标轴指示器，坐标轴触发有效
-        type: 'shadow', // 默认为直线，可选为：'line' | 'shadow'
-      },
-    },
-    xAxis: {
-      type: 'category',
-      data: dateArr.value,
-    },
-    yAxis: {
-      type: 'value',
-    },
-    series: [
-      {
-        name: '体重',
-        type: 'line',
-        stack: 'Total',
-        data: weightArr.value,
-      },
-      {
-        name: '血糖',
-        type: 'line',
-        stack: 'Total',
-        data: bloodSugarArr.value,
-      },
-      {
-        name: '血压(高)',
-        type: 'line',
-        stack: 'Total',
-        data: highBloodPressureArr.value,
-      },
-      {
-        name: '血压(低)',
-        type: 'line',
-        stack: 'Total',
-        data: lowBloodPressureArr.value,
-      },
-    ],
+    state.data = res.data.items
   }
 })
+
+async function tabChange({ paneKey }) {
+  const key = Object.keys(type)[paneKey]
+  chartRef.value.refresh(option)
+  await init(key)
+  chartRef.value.setOption(option)
+
+  // Taro.nextTick(() => {
+  //   setTimeout(() => {
+  //     chartRef.value.refresh(option) // 初始化图表
+  //   }, 500)
+  // })
+}
+
 const chartRef = ref()
 
 Taro.nextTick(() => {
-  // console.log('[ chartRef ] >', chartRef.value)
-
   setTimeout(() => {
-    chartRef.value.refresh(option.value) // 初始化图表
-    // setInterval(() => {
-    //   let firstValue = option.series[0].data.shift()
-    //   option.series[0].data.push(firstValue)
-    //   chartRef.value.setOption(option) // 异步更新图表数据
-    // }, 2000)
+    chartRef.value.refresh(option) // 初始化图表
   }, 200)
 })
 </script>
@@ -105,11 +175,17 @@ Taro.nextTick(() => {
 <style lang="scss">
 .record {
   width: 100%;
-  height: 100vh;
-  padding: 30px 0;
-  .nut-tabpane {
-    padding: 0;
-    height: calc(100vh - 22px);
+  height: calc(100vh - 100px);
+  .toggle {
+    display: flex;
+    height: 60rpx;
+    width: 100%;
+    align-items: center;
+    justify-content: flex-end;
+    padding: 0 10px;
   }
+}
+.nut-tabpane {
+  padding: 0;
 }
 </style>
