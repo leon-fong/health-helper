7f0713383e76c27381f55799b981014b68c7a885
Bright Fong <ooohmydawn@hotmail.com>
2022-05-22 11:04:59 +0800

    chore: update api

--- src/apis/analysis.js
+++ src/apis/analysis.js
@@ -3,8 +3,11 @@ import request from '~@/utils/request.js'
 export function getAnalysis( type) {
   return request.get(`/info/${type}`)
 }
-export function getWeightList() {
-  return request.get('/info/weights')
-}
 
+
+export function getWeightList( page = 1,pageSize = 10) {
+  return request.get('/info/weights',{data:{
+    page,pageSize
+  }})
+}
 
