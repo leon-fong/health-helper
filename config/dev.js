d0629cda5a6d3e27818d1834ca66d56ccea86285
ooohmydawn <ooohmydawn@hotmail.com>
2022-06-05 17:38:07 +0800

    chore: compress

--- config/dev.js
+++ config/dev.js
@@ -5,8 +5,27 @@ module.exports = {
   defineConstants: {
   },
   mini: {
+  
     optimizeMainPackage: {
       enable: true
+    },
+     // fix: https://taro-docs.jd.com/taro/docs/compile-optimized#解决包体积过大无法进行预览的问题
+    webpackChain: (chain, webpack) => {
+      chain.merge({
+        plugin: {
+          install: {
+            plugin: require('terser-webpack-plugin'),
+            args: [{
+              terserOptions: {
+                compress: true, // 默认使用terser压缩
+                // mangle: false,
+                keep_classnames: true, // 不改变class名称
+                keep_fnames: true // 不改变函数名称
+              }
+            }]
+          }
+        }
+      })
     }
   },
   h5: {}
--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export const baseUrl = process.env.NODE_ENV === 'development' ? 
-// 'http://114.96.78.144:8088/health' : './'
-'http://light-x.vaiwan.cn/health': './'
+'http://114.96.78.144:8088/health' : './'
+// 'http://light-x.vaiwan.cn/health': './'
 
