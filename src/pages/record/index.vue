d060df26c70ab419871fa51e8b672d2f1c874581
fanglm <ooohmydawn@hotmail.com>
2023-04-05 18:01:58 +0800

    style: update

--- src/pages/record/index.vue
+++ src/pages/record/index.vue
@@ -4,20 +4,20 @@
       <daily-record />
     </div>
     <div class="bottom">
-      <div class="card box" style="margin-top: 20px; background-color: #f8f4e6" @click="toFoodRecord">
+      <div class="card box food" style="margin-top: 20px;" @click="toFoodRecord">
         <div class="top recommend">
           <div class="title">饮食摄入</div>
           <span class="text">推荐：{{ calorie }}</span>
         </div>
         <div class="value">
           <div class="num">
-            <span class="num-value" style="color: #fbddb2">{{ consume }}</span>
+            <span class="num-value" style="color: #fff">{{ consume }}</span>
             <span class="num-unit">千卡</span>
           </div>
-          <i class="iconfont icon-medicine-bowl" style="color: #fbddb2"></i>
+          <i class="iconfont icon-medicine-bowl" style="color: #fff"></i>
         </div>
       </div>
-      <div class="card box" style="margin-top: 20px" @click="toSportRecord">
+      <div class="card box fitness" style="margin-top: 20px" @click="toSportRecord">
         <div class="top">
           <div class="title">运动消耗</div>
         </div>
@@ -80,6 +80,7 @@ useDidShow(() => {
 Page {
   background-color: #fff;
 }
+
 .record {
   padding: 20px;
 
@@ -87,50 +88,65 @@ Page {
     display: flex;
     justify-content: space-between;
   }
+
   .bottom {
     .title {
       color: #122f38;
       font-weight: bold;
       font-size: 20px;
     }
-    .card {
-      background-color: #f3f9f4;
 
+    .card {
       .recommend {
         display: flex;
         justify-content: space-between;
         align-items: center;
+
         .text {
           font-size: 14px;
           color: #fbddb2;
         }
       }
+
       .value {
         margin-top: 10px;
         text-align: right;
         display: flex;
         justify-content: space-between;
         align-items: center;
+
         .num {
           .num-value {
             font-size: 30px;
             font-weight: bold;
-            color: #d7f0f0;
+            color: #fff;
           }
+
           .num-unit {
             font-size: 14px;
-            color: #ccc;
+            color: #fff;
             margin-left: 5px;
           }
         }
+
         .iconfont {
-          color: #d7f0f0;
+          color: #fff;
           font-size: 70px;
         }
       }
     }
+
+    .food {
+      background-image: radial-gradient(circle farthest-corner at 11.4% 27.8%, rgba(248, 227, 194, 1) 24.3%, rgba(253, 214, 168, 1) 99.8%);
+    }
+
+    .fitness {
+      background-image: radial-gradient(circle farthest-corner at 50.1% 37.3%, rgba(92, 213, 249, 1) 0%, rgba(123, 243, 248, 1) 90.1%);
+    }
+
     .sport {
       background-color: #f8f4e6;
+
       .value {
         margin-top: 10px;
 
